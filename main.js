26b37ed34c1e416fd35e21fd1395c676f76d6327
NiklasLot <nikii17.06@gmail.com>
2020-10-03 01:03:20 +0200

    fixed hover effect on Markdown and Inkscape icon + added one color scheme

--- main.js
+++ main.js
@@ -19,16 +19,16 @@ window.addEventListener('scroll', function () {
   document.getElementById('TechnologiesLink').style.backgroundColor = 'transparent';
   document.getElementById('AcademicAssignmentsLink').style.backgroundColor = 'transparent';
 
-  if (window.scrollY >= elementProjects.offsetTop - 45 && window.scrollY <= elementProjectsBottom - 40) {
+  if (window.scrollY >= elementProjects.offsetTop - 45 && window.scrollY <= elementProjectsBottom - 80) {
     document.getElementById('ProjectsLink').style.backgroundColor = '#222222';
-  } else if (window.scrollY >= elementTechnologies.offsetTop - 45 && window.scrollY <= elementTechnologiesBottom - 40) {
+  } else if (window.scrollY >= elementTechnologies.offsetTop - 100 && window.scrollY <= elementTechnologiesBottom - 40) {
     document.getElementById('TechnologiesLink').style.backgroundColor = '#222222';
   } else if (window.scrollY >= elementAcademicAssignments.offsetTop - 45 && window.scrollY <= elementAcademicAssignmentsBottom - 40) {
     document.getElementById('AcademicAssignmentsLink').style.backgroundColor = '#222222';
   }
 });
 
-/* let buttons = document.querySelectorAll('[id^="btn]'); */
+//Adds effect to the landing button by adding span elements
 const btn = document.getElementById('btn');
 
 btn.addEventListener('mouseenter', function (e) {
@@ -42,4 +42,32 @@ btn.addEventListener('mouseenter', function (e) {
   setTimeout(() => {
     ripples.remove();
   }, 1000);
+});
+
+//Makes the Inkscape-Icon go from one picture (transparent) to the other picture (not transparent)
+const InkscapeIconTransparent = document.getElementById('InkscapeIconTransparent');
+const InkscapeIcon = document.getElementById('InkscapeIcon');
+
+InkscapeIconTransparent.addEventListener('mouseenter', function () {
+  InkscapeIcon.style.opacity = '1';
+  InkscapeIconTransparent.style.opacity = '0';
+
+  InkscapeIconTransparent.addEventListener('mouseleave', function () {
+    InkscapeIcon.style.opacity = '0';
+    InkscapeIconTransparent.style.opacity = '1';
+  });
+});
+
+//Makes the Markdown-Icon go from one picture (transparent) to the other picture (not transparent)
+const MarkdownIconTransparent = document.getElementById('MarkdownIconTransparent');
+const MarkdownIcon = document.getElementById('MarkdownIcon');
+
+MarkdownIconTransparent.addEventListener('mouseenter', function () {
+  MarkdownIcon.style.opacity = '1';
+  MarkdownIconTransparent.style.opacity = '0';
+
+  MarkdownIconTransparent.addEventListener('mouseleave', function () {
+    MarkdownIcon.style.opacity = '0';
+    MarkdownIconTransparent.style.opacity = '1';
+  });
 });
